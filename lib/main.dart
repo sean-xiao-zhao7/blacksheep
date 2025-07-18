306cbdf2c4331d73425e9ca06bc3b11291f18b9e
Jean Paul Xiao <jeanpaulxiao@Jeans-MacBook-Air.local>
2025-07-18 12:35:54 -0400

    Add gradient for home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Text('Sheepfold')));
+  runApp(SheepfoldApp());
+}
+
+class SheepfoldApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [Colors.lightBlue, Colors.purple]),
+          ),
+          child: const Center(child: Text('Sheepfold')),
+        ),
+      ),
+    );
+  }
 }
