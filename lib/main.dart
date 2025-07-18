23d73b5b632aece9da61d863a3db0378f9f020d8
Jean Paul Xiao <jeanpaulxiao@Jeans-MacBook-Air.local>
2025-07-18 12:38:31 -0400

    Add key to main app

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,15 @@ void main() {
 }
 
 class SheepfoldApp extends StatelessWidget {
+  const SheepfoldApp([Key? key]) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Container(
           decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [Colors.lightBlue, Colors.purple]),
+            gradient: LinearGradient(colors: [Colors.lightBlue, Colors.white]),
           ),
           child: const Center(child: Text('Sheepfold')),
         ),
