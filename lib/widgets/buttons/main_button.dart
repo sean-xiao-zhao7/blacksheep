818e8434e23e9919b25222c7f3b268f9f0f3eb02
Jean Paul Xiao <jeanpaulxiao@Jeans-MacBook-Air.local>
2025-07-22 15:02:12 -0400

    Adjust button styles

--- lib/widgets/buttons/main_button.dart
+++ lib/widgets/buttons/main_button.dart
@@ -11,16 +11,18 @@ class MainButton extends StatelessWidget {
       style: ElevatedButton.styleFrom(
         backgroundColor: Colors.white,
         shadowColor: Colors.black,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
       ),
       onPressed: () {
         handler();
       },
       child: Container(
-        padding: EdgeInsets.all(15),
+        padding: EdgeInsets.only(top: 20, bottom: 20, left: 5, right: 5),
         child: Text(
           text,
           style: TextStyle(
-            fontSize: 24,
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
             color: Colors.black,
             fontFamily: 'Now',
           ),
