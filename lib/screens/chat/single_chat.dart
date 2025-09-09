6701d8ebdb74e4f7a4c6cebf72e9c7cda2f1ba2c
Jean Paul Xiao <xiaofeng.zhao@utoronto.ca>
2025-09-09 13:11:43 -0400

    Add chat message entering form

--- lib/screens/chat/single_chat.dart
+++ lib/screens/chat/single_chat.dart
@@ -1,5 +1,4 @@
 import "package:flutter/material.dart";
-import "package:sheepfold/widgets/buttons/main_button.dart";
 
 class SingleChat extends StatefulWidget {
   const SingleChat({super.key});
@@ -15,7 +14,7 @@ class _SingleChatState extends State<SingleChat> {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        color: Colors.white,
+        color: const Color.fromARGB(240, 255, 255, 255),
         borderRadius: BorderRadius.all(Radius.circular(10)),
       ),
       padding: EdgeInsets.all(20),
@@ -23,7 +22,23 @@ class _SingleChatState extends State<SingleChat> {
       child: Column(
         children: [
           Expanded(child: ListView(children: [Text('Dummy chat')])),
-          MainButton('Connect by chat app', () => {}, size: 400),
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Enter chat message',
+              labelStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              floatingLabelBehavior: FloatingLabelBehavior.auto,
+              filled: true,
+              fillColor: Colors.white,
+            ),
+            validator: (value) {
+              if (value == null || value.trim().isEmpty) {
+                return 'First name is required.';
+              }
+              return null;
+            },
+            autocorrect: false,
+            textCapitalization: TextCapitalization.sentences,
+          ),
         ],
       ),
     );
