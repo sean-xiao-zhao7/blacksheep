2ecacb5c4cac5a247bea76cc8f1107c7aa38e65a
Jean Paul Xiao <xiaofeng.zhao@utoronto.ca>
2025-09-29 16:42:29 -0400

    Allow mentor chat list to navigate to single chat

--- lib/screens/chat/mentor_chat_preview_widget.dart
+++ lib/screens/chat/mentor_chat_preview_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:sheepfold/screens/chat/single_chat_screen.dart';
 import 'package:sheepfold/widgets/layouts/headers/now_header.dart';
 
 class MentorChatPreviewWidget extends StatelessWidget {
@@ -8,14 +9,27 @@ class MentorChatPreviewWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print(chatInfo['menteeFirstName']);
     return Container(
       decoration: BoxDecoration(
         color: Colors.white.withAlpha(240),
         borderRadius: BorderRadius.all(Radius.circular(10)),
       ),
       padding: EdgeInsets.all(10),
-      child: NowHeader("Test 2", color: Colors.black, fontSize: 20),
+      child: InkWell(
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (ctx) => SingleChat(
+                messages: chatInfo['messages'],
+                chatId: chatInfo['chatId'],
+                isMentor: true,
+                mentorFirstName: chatInfo['mentorFirstName'],
+              ),
+            ),
+          );
+        },
+        child: NowHeader("Test 2", color: Colors.black, fontSize: 20),
+      ),
     );
   }
 }
