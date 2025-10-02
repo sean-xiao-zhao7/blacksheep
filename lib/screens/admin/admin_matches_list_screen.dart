1062aac668d3953f9c07e1c79a617534fd96dad6
Jean Paul Xiao <xiaofeng.zhao@utoronto.ca>
2025-10-02 14:45:02 -0400

    Add admins dir

---
+++ lib/screens/admin/admin_matches_list_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AdminMatchesListScreen extends StatefulWidget {
+  const AdminMatchesListScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _AdminMatchesListScreenState();
+  }
+}
+
+class _AdminMatchesListScreenState<AdminMatchesListScreen> extends State {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
