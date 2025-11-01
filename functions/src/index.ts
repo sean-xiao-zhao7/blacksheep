c206d56e87f02cda0ee3586e0c97ece59b64cc2b
Jean Paul Xiao <xiaofeng.zhao@utoronto.ca>
2025-11-01 17:37:23 -0400

    Fix functions Ref updating logic

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,27 +5,34 @@
 import { initializeApp } from "firebase-admin/app";
 import { getDatabase } from "firebase-admin/database";
 import { getMessaging } from "firebase-admin/messaging";
-import { onValueCreated } from "firebase-functions/database";
+import { onValueUpdated } from "firebase-functions/database";
 
 initializeApp();
 const db = getDatabase();
 const messaging = getMessaging();
 
-export const sendMentorNewConnectionFCM = onValueCreated(
-    "/chat/{pushId}",
+export const sendMentorNewConnectionFCM = onValueUpdated(
+    "/chats/{chatId}/approved",
     async (event: any) => {
+
+        // get mentorUid
+        const chatRef = db.ref(`/chats/${event.params.chatId}`);
+        let result = await chatRef.get();
+        const chatInfo = result.val();
+
         // get token from users
-        const tokenRef =
-            db.ref(`/users/${event.value['mentorId']}/fcmToken`);
-        const fcmToken = await tokenRef.get();
+        const mentorFCMTokenRef =
+            db.ref(`/users/${chatInfo.mentorUid}/fcmToken`);
+        result = await mentorFCMTokenRef.get();
+        const mentorFCMToken = result.val();
 
         // send FCM
         // App Alert - New Matchup Available - Please Login
         const notification = {
             title: "BlackSheep",
-            body: "New Matchup Available - " + event.value['type'] === 'chat' ? "Please login" : "Check your emails"
+            body: "New Matchup Available - " + chatInfo.type === 'chat' ? "Please login" : "Check your emails"
         };
 
-        await messaging.send({ token: String(fcmToken), notification: notification });
+        await messaging.send({ token: String(mentorFCMToken), notification: notification });
     },
 );
