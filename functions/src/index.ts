dc96f7dfbed75341fe22dcdcbdf150c320a5b393
Jean Paul Xiao <xiaofeng.zhao@utoronto.ca>
2025-11-03 11:48:09 -0500

    Correcty get fcmToken for cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,6 +5,7 @@
 import { initializeApp } from "firebase-admin/app";
 import { getDatabase } from "firebase-admin/database";
 import { getMessaging } from "firebase-admin/messaging";
+import { logger } from "firebase-functions";
 import { onValueUpdated } from "firebase-functions/database";
 
 initializeApp();
@@ -20,11 +21,17 @@ export const sendMentorNewConnectionFCM = onValueUpdated(
         let result = await chatRef.get();
         const chatInfo = result.val();
 
+        // logger.log(chatInfo);
+
         // get token from users
         const mentorFCMTokenRef =
             db.ref(`/users/${chatInfo.mentorUid}/fcmToken`);
+        // /users/KFTXJ70sFCg5CAxzQIkk6FzkMlp1/fcmToken
         result = await mentorFCMTokenRef.get();
         const mentorFCMToken = result.val();
+
+        logger.log(`chatInfo.mentorUid ${chatInfo.mentorUid}`);
+        logger.log(`mentorFCMToken ${mentorFCMToken}`);
 
         // send FCM
         // App Alert - New Matchup Available - Please Login
@@ -34,5 +41,7 @@ export const sendMentorNewConnectionFCM = onValueUpdated(
         };
 
         await messaging.send({ token: String(mentorFCMToken), notification: notification });
+
+        logger.log('Finished.')
     },
 );
